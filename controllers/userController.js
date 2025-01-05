3be0014f7ece2877f5f83154c4694e08e74d3889
sanjeevuiuxdev <sanjeev.uiuxdev@gmail.com>
2025-01-05 22:20:24 +0530

    Switched to bcryptjs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const userModel = require("../models/userModel")
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 var JWT = require('jsonwebtoken');
 const { generateToken } = require("../utils/generateToken");
 const { deleteMediaFromCloudinary, uploadMedia } = require("../utils/cloudinary");
